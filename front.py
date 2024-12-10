0930e38a8ffccf23463ffe9bd013d87fecacdfcd
ferk2414gt <132459134+ferk2414gt@users.noreply.github.com>
2024-12-10 09:56:29 -0600

    Add files via upload

---
+++ front.py
@@ -0,0 +1,36 @@
+import streamlit as st 
+from chat import predict_intent, get_response, intents
+
+st.title("valerio Bot")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+if "first_message" not in st.session_state:
+    st.session_state.first_message = True
+
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+         st.markdown(message["content"])
+
+if st.session_state.first_message:
+    with st.chat_message("assistant"):
+        st.markdown("Hola,¿como puedo ayudarte?")
+
+
+st.session_state.messages.append({"role":"assistant","content":"hola, ¿como puedo ayudarte?"})
+st.session_state.first_message = False       
+
+if prompt := st.chat_input("¿como puedo ayudarte?"):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role":"user","content":prompt})
+    
+#implementar IA
+    insts=predict_intent(prompt)
+    res=get_response(insts,intents) 
+   
+    with st.chat_message("assistant"):
+        st.markdown(res)
+
+    st.session_state.messages.append({"role": "assistant", "content":res}) 
